743606a5d10d4ac7add51b3299574bb05894c788
NguyenDacQuyet <thatao2272006@gmail.com>
2025-09-26 13:48:03 +0700

    Update MAD101 Q1 P1.cpp

--- MAD101 Q1 P1.cpp
+++ MAD101 Q1 P1.cpp
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool thuocTapA(int n) {
+bool tapA(int n) {
     return ((1LL * n * n + 7LL * n + 6) % 21 == 0);
 }
 
 int main() {
     int dem = 0;
     for (int i = 0; i <= 12000; i++) {
-        if (!thuocTapA(i)) dem++;
+        if (!tapA(i)) dem++;
     }
     cout<< dem;
+
 }
