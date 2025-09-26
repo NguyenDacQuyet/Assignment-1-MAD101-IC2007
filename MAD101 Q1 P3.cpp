47a79a0926da9755cc5a2dca2d2061f7167e0db3
NguyenDacQuyet <thatao2272006@gmail.com>
2025-09-26 13:41:54 +0700

    Update MAD101 Q1 P3.cpp

--- MAD101 Q1 P3.cpp
+++ MAD101 Q1 P3.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-bool inC(int n) {
+bool tapC(int n) {
     int cnt = 0;
     for (int p = 2; p * p <= n; p++) {
         if (n % p == 0) {
@@ -17,8 +17,9 @@ bool inC(int n) {
 int main() {
     vector<int> v;
     for (int i = 0; i <= 12000; i++){
-	    if (inC(i)) v.push_back(i);
+	    if (tapC(i)) v.push_back(i);
 	}
 
     cout << v[119];
+
 }
